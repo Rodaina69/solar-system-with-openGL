4675d2f0d28ea0ae04f743be9b8794d961923744
Rodaina69 <73739277+Rodaina69@users.noreply.github.com>
2023-01-10 13:24:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -303,7 +303,7 @@ int main (int argc, char**argv)
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
     glutInitWindowSize(1200,800);
-    glutCreateWindow("solar system' 19101185");
+    glutCreateWindow("solar system' 19101185"); //window title 
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
     glutReshapeFunc(reshape);
